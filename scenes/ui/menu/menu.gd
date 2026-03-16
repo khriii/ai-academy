a894386412c50275f84266f31d8fb30cfeb91f84
khriii <christianstelluti7@gmail.com>
2026-03-16 08:54:23 +0100

    improved menu scene logic

--- scenes/ui/menu/menu.gd
+++ scenes/ui/menu/menu.gd
@@ -8,3 +8,8 @@ extends Control
 func _ready() -> void:
 	start_message_label.text = lang_component.get_lang_text("start_message_label")
 	quit_button.text = lang_component.get_lang_text("quit_button")
+	
+	quit_button.pressed.connect(_on_quit_button_pressed)
+
+func _on_quit_button_pressed():
+	get_tree().quit()
